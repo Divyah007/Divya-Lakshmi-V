53a363206f8c3255f6ba8786ad3b08f5b6955795
Divya Lakshmi V <46861059+Divyah007@users.noreply.github.com>
2021-05-08 19:36:01 +0530

    Delete Divya note keeping application directory

--- Divya note keeping application/main.js
+++
@@ -1,7 +0,0 @@
-const Listed=document.querySelector('#listed');
-
-const url='file:///C:/Users/Divya/Desktop/Note%20keeping%20Application/note-app-master/j.json';
-
-// get- read the post
-fetch(url)
-   .then(res=>console.log(res));
